aac3450d099ba3655add06cc8e22b75f75312512
Marco Nie <github@niekun.net>
2024-09-29 13:43:54 +0800

    update layout

--- hanzhong-loupan-viewer/src/App.js
+++ hanzhong-loupan-viewer/src/App.js
@@ -115,51 +115,57 @@ function App() {
   };
 
   return (
-    <Container fluid>
+    <Container fluid className="px-3 px-md-4">
       <h1 className="text-center my-4">汉中楼盘信息</h1>
       <p className="text-center text-muted">数据更新时间: {scrapeTime}</p>
-      <div className="d-flex justify-content-between mb-3">
-        <Form.Select 
-          style={{width: 'auto'}}
-          value={selectedHouseType}
-          onChange={(e) => setSelectedHouseType(e.target.value)}
-        >
-          {houseTypeOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option === 'all' ? '所有户型' : option}
-            </option>
-          ))}
-        </Form.Select>
-        <Form.Select 
-          style={{width: 'auto'}}
-          value={selectedSalesStatus}
-          onChange={(e) => setSelectedSalesStatus(e.target.value)}
-        >
-          {salesStatusOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option === 'all' ? '所有销售状态' : option}
-            </option>
-          ))}
-        </Form.Select>
-        <Dropdown as={ButtonGroup}>
-          <Dropdown.Toggle variant="primary" id="dropdown-basic">
-            排序方式: {getSortByText(sortBy)}
-          </Dropdown.Toggle>
+      <Row className="mb-3 g-2">
+        <Col xs={12} sm={6} md={4}>
+          <Form.Select 
+            value={selectedHouseType}
+            onChange={(e) => setSelectedHouseType(e.target.value)}
+            className="w-100"
+          >
+            {houseTypeOptions.map((option, index) => (
+              <option key={index} value={option}>
+                {option === 'all' ? '所有户型' : option}
+              </option>
+            ))}
+          </Form.Select>
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <Form.Select 
+            value={selectedSalesStatus}
+            onChange={(e) => setSelectedSalesStatus(e.target.value)}
+            className="w-100"
+          >
+            {salesStatusOptions.map((option, index) => (
+              <option key={index} value={option}>
+                {option === 'all' ? '所有销售状态' : option}
+              </option>
+            ))}
+          </Form.Select>
+        </Col>
+        <Col xs={12} md={4}>
+          <Dropdown as={ButtonGroup} className="w-100">
+            <Dropdown.Toggle variant="primary" id="dropdown-basic" className="w-100">
+              排序方式: {getSortByText(sortBy)}
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={() => sortLoupans('default')}>默认</Dropdown.Item>
-            <Dropdown.Item onClick={() => sortLoupans('price')}>价格</Dropdown.Item>
-            <Dropdown.Item onClick={() => sortLoupans('totalPrice')}>总价</Dropdown.Item>
-            <Dropdown.Item onClick={() => sortLoupans('openDate')}>最新开盘时间</Dropdown.Item>
-            <Dropdown.Item onClick={() => sortLoupans('greenRatio')}>绿化率</Dropdown.Item>
-            <Dropdown.Item onClick={() => sortLoupans('plotRatio')}>容积率</Dropdown.Item>
-            <Dropdown.Item onClick={() => sortLoupans('propertyFee')}>物业费</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
+            <Dropdown.Menu className="w-100">
+              <Dropdown.Item onClick={() => sortLoupans('default')}>默认</Dropdown.Item>
+              <Dropdown.Item onClick={() => sortLoupans('price')}>价格</Dropdown.Item>
+              <Dropdown.Item onClick={() => sortLoupans('totalPrice')}>总价</Dropdown.Item>
+              <Dropdown.Item onClick={() => sortLoupans('openDate')}>最新开盘时间</Dropdown.Item>
+              <Dropdown.Item onClick={() => sortLoupans('greenRatio')}>绿化率</Dropdown.Item>
+              <Dropdown.Item onClick={() => sortLoupans('plotRatio')}>容积率</Dropdown.Item>
+              <Dropdown.Item onClick={() => sortLoupans('propertyFee')}>物业费</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
       <Row>
         {sortedLoupans.map((loupan, index) => (
-          <Col key={index} md={6} lg={4} className="mb-4">
+          <Col key={index} xs={12} md={6} lg={4} className="mb-4">
             <LoupanDisplay loupan={loupan} selectedHouseType={selectedHouseType} />
           </Col>
         ))}
